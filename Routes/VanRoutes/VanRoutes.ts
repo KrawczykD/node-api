b52b5becd1d11d2da37ca188842cae9eb216c65e
Damian Krawczyk <damiankrawczyk23@gmail.com>
2022-10-24 22:30:31 +0100

    create van routes

---
+++ Routes/VanRoutes/VanRoutes.ts
@@ -0,0 +1,64 @@
+const express = require('express');
+
+const router = express.Router();
+
+import DatabaseRepository from '../../Repositories/DatabaseRepository/DatabaseRepository';
+import {connection} from 'mongoose';
+
+import IVan from '../../DatabaseModels/Van/IVan';
+import Van from '../../DatabaseModels/Van/Van';
+
+import UoW from '../../Repositories/VanRepository/UnityOfWork';
+
+const databaseRepository = new DatabaseRepository(connection , "mongodb://localhost:27017/API");
+
+//get all
+router.get('/van', async (req, res) => {
+
+  try {
+    await databaseRepository.initializeDB();
+    const vanUoW = await new UoW(Van);
+    res.json(await vanUoW.getAllVans() as Array<IVan>);
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  } finally{
+    databaseRepository.closeDB();
+  }
+})
+
+//get one by name
+router.get('/van/:registrationNumber', async (req, res) => {
+  try {
+    await databaseRepository.initializeDB();
+    const vanUoW = await new UoW(Van);
+    res.json(await vanUoW.getVanByRegistrationNumber(req.params.registrationNumber) as Array<IVan>);
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  } finally{
+    databaseRepository.closeDB();
+  }
+})
+
+
+// Creating one
+router.post('/van/create', async (req, res) => {
+    //name: req.query.name,
+    const newVan : IVan = {
+        registrationNumber: req.query.registrationNumber,
+        motDueDate: new Date(new Date().getFullYear() + 1),
+        createAt: new Date(),
+        isActive: true,
+        driverId: req.query.driverId
+    }
+    try {
+      await databaseRepository.initializeDB();
+      
+      res.status(201).json(await new Van(newVan).save());
+    } catch (err) {
+      res.status(400).json({ message: err.message })
+    } finally {
+      databaseRepository.closeDB();
+    }
+  })
+  
+  export default router;
